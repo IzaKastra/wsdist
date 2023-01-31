255d06ec48be66a98bc8def2733dec8929bf7750
IzaKastra <87741876+IzaKastra@users.noreply.github.com>
2023-01-31 09:30:25 -0600

    Update get_hitrate.py

--- get_hitrate.py
+++ get_hitrate.py
@@ -18,12 +18,11 @@ def get_hitrate(player_accuracy, ws_acc, enemy_eva, weaponslot, first, skill_typ
     if weaponslot in ["ranged", "sub"] or skill_type in ["Great Sword", "Great Axe", "Great Katana", "Scythe", "Staff", "Polearm"]: # off-hand, 2-handed, and ranged hits cap at 95% hit rate
         hitrate_cap = 0.95
     else:
-        hitrate_cap = 0.99 # TODO: call this hitrate_cap instead
+        hitrate_cap = 0.99
         
     hitrate_check = (75 + 0.5*(player_accuracy + ws_acc + 100*first - enemy_eva))/100
 
     hitrate_floor = 0 if weaponslot=="ranged" else 0.2 # 20% melee hit rate minimum. 0% for ranged
-
     if hitrate_check > hitrate_cap:
         hitrate = hitrate_cap
     elif hitrate_check < hitrate_floor:
