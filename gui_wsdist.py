85205b50b549bb735a344496dc08ad350bb5ea67
IzaKastra <87741876+IzaKastra@users.noreply.github.com>
2023-02-16 15:20:21 -0600

    Starting TP > 0.

--- gui_wsdist.py
+++ gui_wsdist.py
@@ -653,6 +653,7 @@ while True:
             max_tp = int(values["maxtp"]) if int(values["maxtp"]) < 3000 else 3000
             starting_tp = int(values["startingtp"]) if int(values["startingtp"]) <= 999 else 999
             starting_tp = min_tp-1 if starting_tp >= min_tp else starting_tp
+            starting_tp = 0 if starting_tp < 0 else starting_tp
 
 
             fitn = 2 # Fit two slots simultaneously. Hard-coded because 3 isn't worth the time and 1 occasionally results in incorrect sets
