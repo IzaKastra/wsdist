02220fdbff4f868f1485cfc224b8b39eb6957c6b
IzaKastra <87741876+IzaKastra@users.noreply.github.com>
2023-02-15 20:26:47 -0600

    Adjusted minimum ranged hitrate
    New value allows the code to break out of 0% hit rate traps.

--- get_hitrate.py
+++ get_hitrate.py
@@ -2,7 +2,7 @@
 # Created by Kastra on Asura.
 # Feel free to /tell in game or send a PM on FFXIAH you have questions, comments, or suggestions.
 #
-# Version date: 2023 January 21
+# Version date: 2023 February 14
 #
 # This code contains the function used to hit rates using the equation on BG Wiki
 #
@@ -22,12 +22,18 @@ def get_hitrate(player_accuracy, ws_acc, enemy_eva, weaponslot, first, skill_typ
         
     hitrate_check = (75 + 0.5*(player_accuracy + ws_acc + 100*first - enemy_eva))/100
 
-    hitrate_floor = 0 if weaponslot=="ranged" else 0.2 # 20% melee hit rate minimum. 0% for ranged
+    hitrate_floor = 0.2 if weaponslot=="ranged" else 0.2 # 20% melee hit rate minimum. 0% for ranged(?)
+                                                         # I'm using 20% for ranged to force some % of the sets to do damage, which allows the code to "recover" from 0% hit rate and slowly build a 95% hit rate set.
+                                                         # With 20% floor (but true 0%), the code will find the best damage set that has 20% hitrate first
+                                                         # then it will find a "better" set that has more accuracy, putting damage higher at a true hitrate of 21%.
+                                                         # From there, the code can/will build its way to hitrate cap naturally.
+
     if hitrate_check > hitrate_cap:
         hitrate = hitrate_cap
     elif hitrate_check < hitrate_floor:
         hitrate = hitrate_floor
     else:
         hitrate = hitrate_check
+    # print(player_accuracy, ws_acc, enemy_eva, weaponslot, first, skill_type, hitrate)
 
     return(hitrate)
