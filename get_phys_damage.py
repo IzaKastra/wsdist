891600ea951dfcec8cea729c8f629963e17c5451
IzaKastra <87741876+IzaKastra@users.noreply.github.com>
2023-03-02 20:57:47 -0600

    SA/TA/Flourish modifiers moved to after WSD

--- get_phys_damage.py
+++ get_phys_damage.py
@@ -2,7 +2,7 @@
 # Created by Kastra on Asura.
 # Feel free to /tell in game or send a PM on FFXIAH you have questions, comments, or suggestions.
 #
-# Version date: 2022 December 15
+# Version date: 2023 March 02
 #
 # This code contains the function used to calculate physical damage dealt from a single weapon swing.
 #
@@ -14,7 +14,7 @@ def get_phys_damage(wpn_dmg, fstr_wpn, wsc, pdif, ftp, crit, crit_dmg, wsd, ws_b
     # Calculate physical damage dealt for a single attack of a weapon skill. Uses equation from BG wiki
     # https://www.bg-wiki.com/ffxi/Weapon_Skill_Damage
     #
-    phys = int(  ((wpn_dmg + fstr_wpn + wsc)*ftp + sneak_attack_bonus*(n==0) + trick_attack_bonus*(n==0) + climactic_flourish_bonus*(n==0) + striking_flourish_bonus*(n==0) + ternary_flourish_bonus*(n==0)) * pdif * (1 + crit*min(crit_dmg,1.0)) * (1+wsd*(n==0)) * (1+ws_bonus) * (1+ws_trait) ) # crit = True/False
+    phys = int(  ((wpn_dmg + fstr_wpn + wsc)*ftp*(1+wsd*(n==0))*(1+ws_bonus)*(1+ws_trait)) + sneak_attack_bonus*(n==0) + trick_attack_bonus*(n==0) + climactic_flourish_bonus*(n==0) + striking_flourish_bonus*(n==0) + ternary_flourish_bonus*(n==0)) * pdif * (1 + crit*min(crit_dmg,1.0)) # crit = True/False
     return(phys)
 
 @njit
@@ -23,5 +23,5 @@ def get_avg_phys_damage(wpn_dmg, fstr_wpn, wsc, pdif, ftp, crit_rate, crit_dmg, 
     # Calculate average physical damage dealt for a single attack of a weapon skill. Uses equation from BG wiki
     # https://www.bg-wiki.com/ffxi/Weapon_Skill_Damage
     #
-    phys = int(  ((wpn_dmg + fstr_wpn + wsc)*ftp + sneak_attack_bonus + trick_attack_bonus + climactic_flourish_bonus + striking_flourish_bonus + ternary_flourish_bonus) * pdif * (1 + min(crit_rate,1.0)*min(crit_dmg,1.0)) * (1+wsd)*(1+ws_bonus)*(1+ws_trait)  ) # crit_rate is a number between 0 and 1
+    phys = int(  ((wpn_dmg + fstr_wpn + wsc)*ftp  * (1+wsd)*(1+ws_bonus)*(1+ws_trait)) + sneak_attack_bonus + trick_attack_bonus + climactic_flourish_bonus + striking_flourish_bonus + ternary_flourish_bonus) * pdif * (1 + min(crit_rate,1.0)*min(crit_dmg,1.0)) # crit_rate is a number between 0 and 1
     return(phys)
